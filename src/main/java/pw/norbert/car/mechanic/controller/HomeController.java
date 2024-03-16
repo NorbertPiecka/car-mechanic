2c1b5df6ba1bffc9971d1275c2c134c312dc305d
NorbertPiecka <norbert.piecka9@gmail.com>
2024-03-16 15:55:14 +0100

    Add news pages

--- src/main/java/pw/norbert/car/mechanic/controller/HomeController.java
+++ src/main/java/pw/norbert/car/mechanic/controller/HomeController.java
@@ -29,6 +29,12 @@ public class HomeController {
     public String showNewsDetails(Model model, @PathVariable int newsId) {
         News news = newsRepository.findById(newsId).get();
         model.addAttribute("news", news);
+        model.addAttribute("loremIpsum", generateLoremIpsum());
         return "news";
+    }
+
+    private String generateLoremIpsum() {
+        String loremIpsum = "    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        return loremIpsum;
     }
 }
